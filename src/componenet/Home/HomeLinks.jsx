a5f05e134b93c247dc9de409e95a55d34f535139
afteralexxo <itsalextwod@gmail.com>
2022-12-16 22:25:20 +0300

    add social links

--- src/componenet/Home/HomeLinks.jsx
+++ src/componenet/Home/HomeLinks.jsx
@@ -3,10 +3,10 @@ export default function HomeLinks(){
     return (
         <div className="home-links">
             <ul className='link-container'>
-           <li><a href="" className="link-item"><i className="fa fa-instagram"></i></a></li>
-           <li><a href="https://github.com/afteralexxo" className="link-item"><i className="fa fa-github"></i></a></li>
-           <li><a href="" className="link-item"><i className="fa fa-twitter"></i></a></li>
-           <li><a href="" className="link-item"><i className="fa fa-facebook"></i></a></li>
+           <li><a href="https://instagram.com/alexundertwod/" className="link-item"><i className="fa fa-instagram"></i></a></li>
+           <li><a href="https://github.com/afteralexxo/" className="link-item"><i className="fa fa-github"></i></a></li>
+           <li><a href="https://twitter.com/creativetwod/" className="link-item"><i className="fa fa-twitter"></i></a></li>
+           <li><a href="https://facebook.com/100037514192921/" className="link-item"><i className="fa fa-facebook"></i></a></li>
             </ul>
             <button className='btn-mail'>Email me</button>
         </div>
