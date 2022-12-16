13ee41b8099a075c9441ffeedd50806eb60d9ba1
afteralexxo <itsalextwod@gmail.com>
2022-12-16 22:11:13 +0300

    fix mail button

--- src/componenet/Home/HomeLinks.jsx
+++ src/componenet/Home/HomeLinks.jsx
@@ -4,11 +4,11 @@ export default function HomeLinks(){
         <div className="home-links">
             <ul className='link-container'>
            <li><a href="" className="link-item"><i className="fa fa-instagram"></i></a></li>
-           <li><a href="http://gihub.com/afteralexxo" className="link-item"><i className="fa fa-github"></i></a></li>
+           <li><a href="https://github.com/afteralexxo" className="link-item"><i className="fa fa-github"></i></a></li>
            <li><a href="" className="link-item"><i className="fa fa-twitter"></i></a></li>
            <li><a href="" className="link-item"><i className="fa fa-facebook"></i></a></li>
             </ul>
-            <button>Email me</button>
+            <button className='btn-mail'>Email me</button>
         </div>
     )
 }
