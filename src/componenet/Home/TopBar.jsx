f437b6eaa54d5f3accd1744fd9f3be34916e77f8
afteralexxo <itsalextwod@gmail.com>
2022-12-18 16:23:44 +0300

    add logo

--- src/componenet/Home/TopBar.jsx
+++ src/componenet/Home/TopBar.jsx
@@ -1,13 +1,13 @@
 import './TopBar.css'
 import { useState, useLayoutEffect, useRef } from 'react'
+import AlexLogo from './../../assets/logo2.jpeg'
 
 export default function TopBar (props) {
 
     return (
         <div className="topbar">
             <a className="logo">
-                <span className="first-name">Alemayehu</span>
-                {/* <span className='nick-name'>hu</span> */}
+                <img src={AlexLogo} alt="" width="200px" height="100%" />
             </a>
             <nav className="main-menu">
                 <a href="" className="nav-item">Home</a>
